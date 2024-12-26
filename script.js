8be95f8e13bda50dbef0e32bb6b71f0b3d59b7df
Saiinaruto <saipraveenabburi7@gmail.com>
2024-12-26 09:11:16 +0530

    2

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+let loginBtn = document.getElementById("login");
+      loginBtn.addEventListener("click", () => {
+        loginBtn.innerText === "SignUp"
+          ? (loginBtn.innerHTML = "Login")
+          : (loginBtn.innerHTML = "SignUp");
+      });
+      let cart = document.getElementById("cart");
+      cart.addEventListener("click", () => {
+        if (
+          document.getElementsByClassName("cart")[0].style.display === "block"
+        ) {
+          document.getElementsByClassName("cart")[0].style.display = "none";
+        } else {
+          document.getElementsByClassName("cart")[0].style.display = "block";
+        }
+      });
